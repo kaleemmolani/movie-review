b8271a6f5213fae4e23670202e75c44f01df8acc
kaleem <kaleem@stackpro.in>
2019-08-08 16:39:31 +0530

    Revert "Initial commit"
    This reverts commit d232b50d05caa03693e20220870069544d78312f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MovieListComponent } from './movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MovieListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
